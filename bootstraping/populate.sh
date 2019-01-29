0fed72497f3c0dc8da776886506366450806acfb
Inaki Villar <inaki.seri@gmail.com>
2019-01-29 08:34:39 +0700

    adding one more gradle version in the Bootstraping

--- bootstraping/populate.sh
+++ bootstraping/populate.sh
@@ -9,6 +9,10 @@ cd gradle-profiler
 export PATH=$PATH:build/install/gradle-profiler/bin
 cd ..
 
-gradle-profiler --benchmark --project-dir . --scenario-file  bootstraping/scenario
+git clone https://github.com/cdsap/TalaiotClientExample.git
+
+
+
+gradle-profiler --benchmark --project-dir ./TalaiotClientExample --scenario-file  bootstraping/scenario
 
 
