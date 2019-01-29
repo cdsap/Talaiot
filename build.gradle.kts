feed234a5a983a4c6d98d1f76668c483acb50b23
Inaki Villar <inaki.seri@gmail.com>
2019-01-29 10:05:58 +0700

    Removing dependencies of the android plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,6 @@ buildscript {
         mavenLocal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.2.1")
-        classpath("com.cdsap:talaiot-gradle-4:0.1.8.5-SNAPSHOT")
         classpath(kotlin("gradle-plugin", version = "1.3.11"))
     }
 }
