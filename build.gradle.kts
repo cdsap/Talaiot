0cc3bf352f8f5a659f0f9349e3b2506db28a3f84
Inaki Villar <inaki.seri@gmail.com>
2019-01-29 17:21:56 +0700

    Adding Novoda-Bintray plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,9 @@
-//
-//buildscript {
-//    repositories {
-//        google()
-//        jcenter()
-//        mavenLocal()
-//    }
-//    dependencies {
-//        classpath(kotlin("gradle-plugin", version = "1.3.11"))
-//    }
-//}
-//
-//repositories {
-//    google()
-//    jcenter()
-//    mavenLocal()
-//    mavenCentral()
-//    maven(url = "https://dl.bintray.com/kotlin/ktor")
-//    gradlePluginPortal()
-//
-//}
+
+buildscript {
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        classpath("com.novoda:bintray-release:0.9")
+    }
+}
