36b2a4cdb23537bba20c5d3532ffbaf435421731
Inaki Villar <inaki.seri@gmail.com>
2019-01-29 14:27:25 +0700

    git metrics catch

--- talaiot/src/main/kotlin/com/cdsap/talaiot/metrics/GitMetrics.kt
+++ talaiot/src/main/kotlin/com/cdsap/talaiot/metrics/GitMetrics.kt
@@ -7,7 +7,7 @@ import java.lang.IllegalStateException
 class GitMetrics : Metrics {
     override fun get(): Map<String, String> {
         val runtime = Runtime.getRuntime()
-
+        val gitMetrics = mutableMapOf<String, String>()
         try {
             val bufferBranch = BufferedReader(
                 InputStreamReader(runtime.exec("git rev-parse --abbrev-ref HEAD").inputStream)
@@ -15,22 +15,24 @@ class GitMetrics : Metrics {
             val bufferUserName = BufferedReader(
                 InputStreamReader(runtime.exec("git config --get user.name").inputStream)
             )
-            val user = if (bufferUserName.readLine() != null) {
-                bufferUserName.readLine().trimSpaces()
-            } else ""
+            val userName = bufferUserName.runCatching { readLine() }
+            if (userName.isSuccess) {
+                gitMetrics["gitUser"] = userName.getOrElse { "" }
+            }
+            val branch = bufferBranch.runCatching { readLine() }
+            if (branch.isSuccess) {
+                gitMetrics["branch"] = branch.getOrElse { "" }
 
-            val branch = if (bufferBranch.readLine() != null) {
-                bufferBranch.readLine()
-            } else ""
-
-            return mapOf("gitUser" to user, "branch" to branch)
+            }
+            return gitMetrics
         } catch (e: IllegalStateException) {
             throw IllegalArgumentException(
                 "Error getting information about the Git Repository. In case " +
                         "you are not in a Git Repository you can disable the metrics of Git with " +
                         "talaiot{" +
                         "  gitMetrics=false" +
-                        "}"
+                        "} " +
+                        "${e.message}"
             )
         }
 
