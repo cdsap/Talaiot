fd3dcd45f1bb2adfbffda503339703ef31c3a4ec
Inaki Villar <inaki.seri@gmail.com>
2019-02-23 18:08:07 +0700

    using StartParameters to get metrics from Gradle

--- talaiot/src/main/kotlin/com/cdsap/talaiot/metrics/GradleMetrics.kt
+++ talaiot/src/main/kotlin/com/cdsap/talaiot/metrics/GradleMetrics.kt
@@ -2,21 +2,19 @@ package com.cdsap.talaiot.metrics
 
 import org.gradle.api.Project
 
+
 class GradleMetrics(private val project: Project) : Metrics {
 
     override fun get(): Map<String, String> {
         val gradleMetrics = mutableMapOf<String, String>()
-        if (hasProperty("org.gradle.caching")) {
-            gradleMetrics["gradleCaching"] = property("org.gradle.caching") as String
+        project.gradle.startParameter.apply {
+            gradleMetrics["gradleCaching"] = "${this.isBuildCacheEnabled}"
+            gradleMetrics["gradleParallel"] = "${this.isParallelProjectExecutionEnabled}"
+            gradleMetrics["gradleProfile"] = "${this.isProfile}"
+            gradleMetrics["gradleOffline"] = "${this.isOffline}"
         }
         if (hasProperty("org.gradle.daemon")) {
             gradleMetrics["gradleDaemon"] = property("org.gradle.daemon") as String
-        }
-        if (hasProperty("org.gradle.parallel")) {
-            gradleMetrics["gradleParallel"] = property("org.gradle.parallel") as String
-        }
-        if (hasProperty("org.gradle.configureondemand")) {
-            gradleMetrics["gradleConfigurationOnDemand"] = property("org.gradle.configureondemand") as String
         }
         gradleMetrics["gradleVersion"] = project.gradle.gradleVersion
         return gradleMetrics
