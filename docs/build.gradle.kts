27caba79dc1da943be9227e9526408beaba0da44
Inaki Villar <inaki.seri@gmail.com>
2019-05-05 12:42:03 +0700

    dry debug for orchid

--- docs/build.gradle.kts
+++ docs/build.gradle.kts
@@ -18,7 +18,8 @@ orchid {
     theme = "Editorial"
     baseUrl = ""
     version = "1.0.0"
-    githubToken = "71d7feac78b0f460fb514f6c92d0b928dc937391"
+    githubToken = ""
+    dryDeploy = "true"
     // srcDir  = "../talaiot/src/main/kotlin"
 
 
