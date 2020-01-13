4da788be44f08e2b0f31f160eee3d82e2b33a2b1
Inaki Villar <inaki.seri@gmail.com>
2020-01-12 17:41:41 -0800

    including Custom Publisher in the sample

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,4 +1,6 @@
 import com.cdsap.talaiot.logger.LogTracker
+import com.cdsap.talaiot.publisher.Publisher
+import com.cdsap.talaiot.entities.ExecutionReport
 
 repositories {
     jcenter()
@@ -12,8 +14,8 @@ plugins {
     id("talaiot") version "1.0.11-SNAPSHOT"
 }
 
-dependencies{
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.3.60")
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.60")
 }
 
 talaiot {
@@ -30,5 +32,13 @@ talaiot {
             taskMetricName = "task"
             buildMetricName = "build"
         }
+        customPublisher = CustomPublisher()
+    }
+}
+
+class CustomPublisher : Publisher {
+
+    override fun publish(report: ExecutionReport) {
+        println("[CustomPublisher] : Number of tasks = ${report.tasks?.size}")
     }
 }
