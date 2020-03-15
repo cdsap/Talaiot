08ec4373308f4bd9081dc963b06f66e9fe90aa6e
Inaki Villar <inaki.seri@gmail.com>
2020-03-14 20:43:30 -0700

    fixing test MetricsConfiguration

--- talaiot/src/test/kotlin/com/cdsap/talaiot/metrics/MetricsConfigurationTest.kt
+++ talaiot/src/test/kotlin/com/cdsap/talaiot/metrics/MetricsConfigurationTest.kt
@@ -6,52 +6,54 @@ import io.kotlintest.specs.BehaviorSpec
 
 class MetricsConfigurationTest : BehaviorSpec({
     given("metrics configuration") {
-
         `when`("defaults are used") {
             val metricsConfiguration = MetricsConfiguration()
             val metrics = metricsConfiguration.build()
-
-            assert(metrics.count { it is RootProjectNameMetric } == 1)
-            assert(metrics.count { it is GradleRequestedTasksMetric } == 1)
-            assert(metrics.count { it is GradleVersionMetric } == 1)
+            then("RootProject, GradleRequested and GradleVersion are included") {
+                assert(metrics.count { it is RootProjectNameMetric } == 1)
+                assert(metrics.count { it is GradleRequestedTasksMetric } == 1)
+                assert(metrics.count { it is GradleVersionMetric } == 1)
+            }
         }
-        
         `when`("environment metrics are configured") {
             val metricsConfiguration = MetricsConfiguration()
             val metrics = metricsConfiguration.environment().build()
-            assert(metrics.count { it is HostnameMetric } == 1)
-            assert(metrics.count { it is OsManufacturerMetric } == 1)
-            assert(metrics.count { it is PublicIpMetric } == 1)
-            assert(metrics.count { it is DefaultCharsetMetric } == 1)
+            then("HostnameMetric, OsManufacturerMetric, PublicIpMetric and DefaultCharsetMetric are included") {
+                assert(metrics.count { it is HostnameMetric } == 1)
+                assert(metrics.count { it is OsManufacturerMetric } == 1)
+                assert(metrics.count { it is PublicIpMetric } == 1)
+                assert(metrics.count { it is DefaultCharsetMetric } == 1)
+            }
         }
-
         `when`("performance metrics are configured") {
             val metricsConfiguration = MetricsConfiguration()
             val metrics = metricsConfiguration.performance().build()
-
-            assert(metrics.count { it is ProcessorCountMetric } == 1)
+            then("ProcessorCountMetric is included") {
+                assert(metrics.count { it is ProcessorCountMetric } == 1)
+            }
         }
-
         `when`("git metrics are configured") {
             val metricsConfiguration = MetricsConfiguration()
             val metrics = metricsConfiguration.git().build()
-
-            assert(metrics.count { it is GitBranchMetric } == 1 &&
-                    metrics.count { it is GitUserMetric } == 1)
+            then("GitBranchMetric and GitUserMetric are included") {
+                assert(metrics.count { it is GitBranchMetric } == 1 &&
+                        metrics.count { it is GitUserMetric } == 1)
+            }
         }
-
         `when`("build Id generation is disabled in the default behaviour") {
             val metricsConfiguration = MetricsConfiguration()
             val metrics = metricsConfiguration.performance().build()
-
-            assert(metrics.count { it is BuildIdMetric } == 0)
+            then("BuildIdMetric is disabled") {
+                assert(metrics.count { it is BuildIdMetric } == 0)
+            }
         }
         `when`("build Id generation is enabled") {
             val metricsConfiguration = MetricsConfiguration()
             metricsConfiguration.generateBuildId = true
             val metrics = metricsConfiguration.performance().build()
-
-            assert(metrics.count { it is BuildIdMetric } == 1)
+            then("BuildIdMetric is included") {
+                assert(metrics.count { it is BuildIdMetric } == 1)
+            }
         }
     }
 })
