511da14a84278448832308f5922dfe9a23e5318d
Mozart Petter <mozart@petter.io>
2020-04-08 20:59:40 +0900

    Update sample with new examples

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,6 +1,8 @@
-import com.cdsap.talaiot.logger.LogTracker
-import com.cdsap.talaiot.publisher.Publisher
 import com.cdsap.talaiot.entities.ExecutionReport
+import com.cdsap.talaiot.logger.LogTracker
+import com.cdsap.talaiot.metrics.HostnameMetric
+import com.cdsap.talaiot.metrics.SimpleMetric
+import com.cdsap.talaiot.publisher.Publisher
 
 repositories {
     jcenter()
@@ -15,7 +17,7 @@ plugins {
 }
 
 dependencies {
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.3.60")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.60")
 }
 
 talaiot {
@@ -34,11 +36,41 @@ talaiot {
         }
         customPublisher = CustomPublisher()
     }
+
+    metrics {
+        // Talaiot provides a few methods to add a group of metrics at once:
+        default()
+        git()
+
+        // You can also add your own custom Metric objects:
+        customMetrics(
+            HelloMetric(),
+            // Including some of the provided metrics, individually.
+            HostnameMetric()
+        )
+
+        // Or define build or task metrics directly:
+        customBuildMetrics(
+            "kotlin" to "1.4",
+            "java" to "8"
+        )
+        customTaskMetrics(
+            "pid" to "2134"
+        )
+    }
 }
 
 class CustomPublisher : Publisher {
-
     override fun publish(report: ExecutionReport) {
         println("[CustomPublisher] : Number of tasks = ${report.tasks?.size}")
+        println("[CustomPublisher] : HelloMetric = ${report.customProperties.buildProperties["hello"]}")
+        println("[CustomPublisher] : Kotlin = ${report.customProperties.buildProperties["kotlin"]}")
+        println("[CustomPublisher] : Java = ${report.customProperties.buildProperties["java"]}")
+        println("[CustomPublisher] : PID = ${report.customProperties.taskProperties["pid"]}")
     }
 }
+
+class HelloMetric : SimpleMetric<String>(
+    provider = { "Hello!" },
+    assigner = { report, value -> report.customProperties.buildProperties["hello"] = value }
+)
