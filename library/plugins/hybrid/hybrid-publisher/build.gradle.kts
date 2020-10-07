54ad23099ec00814fd44d9794a26de2ad549116a
Inaki Villar <inaki.villar@gotinder.com>
2020-10-06 18:34:18 -0700

    changing to testImplementation

--- library/plugins/hybrid/hybrid-publisher/build.gradle.kts
+++ library/plugins/hybrid/hybrid-publisher/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":library:talaiot"))
-    implementation("com.rethinkdb:rethinkdb-driver:2.3.3")
-    implementation("org.influxdb:influxdb-java:2.19")
+    testImplementation("com.rethinkdb:rethinkdb-driver:2.3.3")
+    testImplementation("org.influxdb:influxdb-java:2.19")
     testImplementation(project(":library:talaiot-test-utils"))
 }
