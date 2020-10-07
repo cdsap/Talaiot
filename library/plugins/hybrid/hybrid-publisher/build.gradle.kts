16b51c413f9779f5f2828ba6f92e4e898e6ab2ae
Inaki Villar <inaki.villar@gotinder.com>
2020-10-06 18:33:58 -0700

    missing dependencies in hybrid

--- library/plugins/hybrid/hybrid-publisher/build.gradle.kts
+++ library/plugins/hybrid/hybrid-publisher/build.gradle.kts
@@ -4,5 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":library:talaiot"))
+    implementation("com.rethinkdb:rethinkdb-driver:2.3.3")
+    implementation("org.influxdb:influxdb-java:2.19")
     testImplementation(project(":library:talaiot-test-utils"))
 }
