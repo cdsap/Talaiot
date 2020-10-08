e2cf52c674cd3d824b70452cb3ca0d9347c4d0fc
Mozart Petter <mozart@petter.io>
2020-10-08 23:27:03 +0900

    Add plugin configuration

--- library/plugins/graph/graph-plugin/build.gradle.kts
+++ library/plugins/graph/graph-plugin/build.gradle.kts
@@ -2,6 +2,14 @@ plugins {
     id("talaiotPlugin")
 }
 
+talaiotPlugin {
+    idPlugin = "com.cdsap.talaiot.plugin.graph"
+    artifact = "graph"
+    group = "com.cdsap.talaiot.plugin"
+    mainClass = "com.cdsap.talaiot.plugin.graph.TalaiotGraphPlugin"
+    version = "1.3.6-SNAPSHOT"
+}
+
 dependencies {
     implementation(project(":library:talaiot"))
     implementation(project(":library:plugins:graph:graph-publisher"))
