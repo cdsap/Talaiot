4acc074f4ab399f4f886d7e947c91cd6102230d0
Inaki Villar <inaki.seri@gmail.com>
2020-10-14 09:54:16 -0700

    integrating elasticsearch publisher in hybrid

--- library/plugins/hybrid/hybrid-publisher/src/main/kotlin/com/cdsap/talaiot/publisher/hybrid/HybridPublisher.kt
+++ library/plugins/hybrid/hybrid-publisher/src/main/kotlin/com/cdsap/talaiot/publisher/hybrid/HybridPublisher.kt
@@ -3,9 +3,10 @@ package com.cdsap.talaiot.publisher.hybrid
 import com.cdsap.talaiot.configuration.*
 import com.cdsap.talaiot.entities.ExecutionReport
 import com.cdsap.talaiot.logger.LogTracker
-import com.cdsap.talaiot.publisher.ElasticSearchPublisher
 
 import com.cdsap.talaiot.publisher.Publisher
+import com.cdsap.talaiot.publisher.elasticsearch.ElasticSearchPublisher
+import com.cdsap.talaiot.publisher.elasticsearch.ElasticSearchPublisherConfiguration
 import com.cdsap.talaiot.publisher.influxdb.InfluxDbPublisher
 import com.cdsap.talaiot.publisher.influxdb.InfluxDbPublisherConfiguration
 import com.cdsap.talaiot.publisher.pushgateway.PushGatewayFormatter
