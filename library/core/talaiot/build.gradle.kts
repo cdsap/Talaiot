c50f19fc0ae4c9862c60bdc028c4d0e14bbd8d6e
Mozart Petter <mozart@petter.io>
2020-11-01 12:01:58 +0900

    Remove graphviz dependency from Talaiot core

--- library/core/talaiot/build.gradle.kts
+++ library/core/talaiot/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 dependencies {
     api(project(":library:core:talaiot-logger"))
     api(project(":library:core:talaiot-request"))
-    api("guru.nidi:graphviz-java:0.8.3")
     implementation("com.github.oshi:oshi-core:3.13.3")
     testImplementation("io.github.rybalkinsd:kohttp:0.10.0")
     testImplementation(project(":library:core:talaiot-test-utils"))
--- library/plugins/graph/graph-publisher/build.gradle.kts
+++ library/plugins/graph/graph-publisher/build.gradle.kts
@@ -5,5 +5,6 @@ plugins {
 
 dependencies {
     implementation(project(":library:core:talaiot"))
+    implementation("guru.nidi:graphviz-java:0.8.3")
     testImplementation(project(":library:core:talaiot-test-utils"))
 }
