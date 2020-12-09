aa73b2ba697c894bf5f9d4cf9bdc9f2d54faba1a
Inaki Villar <inaki.seri@gmail.com>
2020-12-08 18:41:13 -0800

    dropping support for Gradle < 6

--- library/plugins/talaiot-standard/src/test/kotlin/com/cdsap/talaiot/IntegrationSpec.kt
+++ library/plugins/talaiot-standard/src/test/kotlin/com/cdsap/talaiot/IntegrationSpec.kt
@@ -13,8 +13,7 @@ import java.io.File
 class DefaultConfigurationSpec : StringSpec({
     "given default config" {
         forAll(listOf(
-            "6.7",
-            "6.5",
+            "6.4.1",
             "6.2.1",
             "6.0.1"
         )) { version: String ->
