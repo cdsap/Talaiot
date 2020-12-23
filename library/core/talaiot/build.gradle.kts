e4bdb76f8aa55ef466f6f8e068b0908664b50042
Inaki Villar <inaki.seri@gmail.com>
2020-12-22 16:50:43 -0800

    updating oshi library

--- library/core/talaiot/build.gradle.kts
+++ library/core/talaiot/build.gradle.kts
@@ -13,7 +13,7 @@ talaiotLib {
 dependencies {
     api(project(":library:core:talaiot-logger"))
     api(project(":library:core:talaiot-request"))
-    implementation("com.github.oshi:oshi-core:3.13.3")
+    implementation("com.github.oshi:oshi-core:5.3.7")
     testImplementation("io.github.rybalkinsd:kohttp:0.10.0")
     testImplementation(project(":library:core:talaiot-test-utils"))
 }
