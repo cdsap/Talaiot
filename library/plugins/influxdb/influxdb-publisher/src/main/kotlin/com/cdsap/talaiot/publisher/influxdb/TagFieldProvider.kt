d6c3ade8ce65d818bb06b9e80e9563aba356229a
Inaki Villar <inaki.seri@gmail.com>
2021-02-23 20:30:40 -0800

    adding tests TagFieldProvider

--- library/plugins/influxdb/influxdb-publisher/src/main/kotlin/com/cdsap/talaiot/publisher/influxdb/TagFieldProvider.kt
+++ library/plugins/influxdb/influxdb-publisher/src/main/kotlin/com/cdsap/talaiot/publisher/influxdb/TagFieldProvider.kt
@@ -5,6 +5,7 @@ import com.cdsap.talaiot.entities.Environment
 import com.cdsap.talaiot.entities.ExecutionReport
 import com.cdsap.talaiot.metrics.BuildMetrics
 import com.cdsap.talaiot.metrics.DefaultBuildMetricsProvider
+import java.lang.IllegalArgumentException
 
 class TagFieldProvider(
     private val executionReport: ExecutionReport,
@@ -20,18 +21,19 @@ class TagFieldProvider(
 
     fun fields(): Map<String, Any> {
         val tags = tags()
-        return buildMetrics.filter {
-            !tags.containsKey(it.key)
-        }
+        return buildMetrics.filter { !tags.containsKey(it.key) }
     }
 
     private fun customMetrics(key: String) =
         tagsConfiguration.contains(BuildMetrics.Custom) &&
                 executionReport.customProperties.buildProperties.contains(key)
 
-
     private fun defaultMetrics(key: String) =
-        tagsConfiguration.contains(BuildMetrics.valueOf(key.capitalize()))
+        try {
+            tagsConfiguration.contains(BuildMetrics.valueOf(key.capitalize()))
+        } catch (e: IllegalArgumentException){
+            false
+        }
 }
 
 
---
+++ library/plugins/influxdb/influxdb-publisher/src/test/kotlin/com/cdsap/talaiot/publisher/influxdb/TagFieldProviderTest.kt
@@ -0,0 +1,95 @@
+package com.cdsap.talaiot.publisher.influxdb
+
+import com.cdsap.talaiot.entities.*
+import com.cdsap.talaiot.utils.TestExecutor
+import com.cdsap.talaiot.logger.TestLogTrackerRecorder
+import com.cdsap.talaiot.metrics.BuildMetrics
+import io.kotlintest.Spec
+import io.kotlintest.specs.BehaviorSpec
+import org.influxdb.dto.Query
+import org.testcontainers.influxdb.KInfluxDBContainer
+
+class TagFieldProviderTest : BehaviorSpec() {
+
+    init {
+        given("TagFieldProvider Instance") {
+
+            `when`("No tags included in the configuration") {
+                val metrics = mutableMapOf("Metrics1" to "value1", "Metrics2" to "value2")
+                val customProperties = CustomProperties()
+                customProperties.buildProperties = metrics
+
+                val executionReport = ExecutionReport(
+                    customProperties = customProperties,
+                    environment = Environment(
+                        cpuCount = "12"
+                    )
+                )
+
+                val tagFieldProvider = TagFieldProvider(
+                    executionReport,
+                    emptyList()
+                )
+
+                then("tags are empty and fields contain CpuCount build metrics + 2 custom properties") {
+                    assert(tagFieldProvider.tags().isEmpty())
+                    assert(tagFieldProvider.fields().containsKey(BuildMetrics.CpuCount.toString()))
+                    assert(tagFieldProvider.fields().containsKey("Metrics1"))
+                    assert(tagFieldProvider.fields().containsKey("Metrics2"))
+                }
+            }
+
+            `when`("tag CpuCount included in the configuration") {
+                val metrics = mutableMapOf("Metrics1" to "value1", "Metrics2" to "value2")
+                val customProperties = CustomProperties()
+                customProperties.buildProperties = metrics
+
+                val executionReport = ExecutionReport(
+                    customProperties = customProperties,
+                    environment = Environment(
+                        cpuCount = "12"
+                    )
+                )
+
+                val tagFieldProvider = TagFieldProvider(
+                    executionReport,
+                    listOf(BuildMetrics.CpuCount)
+                )
+
+                then("tags includes CpuCount") {
+                    assert(tagFieldProvider.tags().size == 1)
+                    assert(tagFieldProvider.tags().containsKey(BuildMetrics.CpuCount.toString()))
+                    assert(!tagFieldProvider.fields().containsKey(BuildMetrics.CpuCount.toString()))
+                }
+            }
+
+            `when`("tags include Custom Metrics") {
+                val metrics = mutableMapOf("Metrics1" to "value1", "Metrics2" to "value2")
+                val customProperties = CustomProperties()
+                customProperties.buildProperties = metrics
+
+                val executionReport = ExecutionReport(
+                    customProperties = customProperties,
+                    environment = Environment(
+                        cpuCount = "12"
+                    )
+                )
+
+                val tagFieldProvider = TagFieldProvider(
+                    executionReport,
+                    listOf(BuildMetrics.Custom)
+                )
+
+                then("tags includes CpuCount") {
+                    assert(tagFieldProvider.tags().size == 2)
+                    assert(tagFieldProvider.tags().containsKey("Metrics1"))
+                    assert(tagFieldProvider.tags().containsKey("Metrics2"))
+                    assert(!tagFieldProvider.tags().containsKey(BuildMetrics.CpuCount.toString()))
+                    assert(!tagFieldProvider.fields().containsKey("Metrics1"))
+                    assert(!tagFieldProvider.fields().containsKey("Metrics2"))
+                    assert(tagFieldProvider.fields().containsKey(BuildMetrics.CpuCount.toString()))
+                }
+            }
+        }
+    }
+}
