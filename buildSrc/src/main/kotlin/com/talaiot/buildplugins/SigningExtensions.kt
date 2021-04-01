10ede0c4d0d413db9d9ef7319dfd5e8441be3ca0
Inaki Villar <inaki.seri@gmail.com>
2021-03-31 17:59:40 -0700

    adding correct path

--- buildSrc/src/main/kotlin/com/talaiot/buildplugins/SigningExtensions.kt
+++ buildSrc/src/main/kotlin/com/talaiot/buildplugins/SigningExtensions.kt
@@ -9,7 +9,7 @@ fun Project.setUpSigning(vararg publications: String) {
     System.getenv("SIGNING_PASSWORD")?.let {
         extra.set("signing.keyId", System.getenv("SIGNING_KEY"))
         extra.set("signing.password", System.getenv("SIGNING_PASSWORD"))
-        extra.set("signing.secretKeyRingFile", System.getenv("SIGNING_FILE"))
+        extra.set("signing.secretKeyRingFile", "${rootProject.rootDir}/${System.getenv("SIGNING_FILE")}")
         configure<SigningExtension> {
             publications.forEach {
                 sign(publication(it))
