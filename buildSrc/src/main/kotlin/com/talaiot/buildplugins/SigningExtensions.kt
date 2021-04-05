602dedf664d926194483d8b9b37a9a686c776440
Inaki Villar <inaki.seri@gmail.com>
2021-04-04 19:38:57 -0700

    ignoring signing in buildSrc Plugin Test

--- buildSrc/src/main/kotlin/com/talaiot/buildplugins/SigningExtensions.kt
+++ buildSrc/src/main/kotlin/com/talaiot/buildplugins/SigningExtensions.kt
@@ -9,12 +9,12 @@ fun Project.setUpSigning(vararg publications: String) {
     System.getenv("SIGNING_PASSWORD")?.let {
         extra.set("signing.keyId", System.getenv("SIGNING_KEY"))
         extra.set("signing.password", System.getenv("SIGNING_PASSWORD"))
-        extra.set("signing.secretKeyRingFile", "${rootProject.rootDir}/${System.getenv("SIGNING_FILE")}")
+        extra.set("signing.secretKeyRingFile", "${rootProject.rootDir}/.signing/secring.gpg")
+
         configure<SigningExtension> {
             publications.forEach {
                 sign(publication(it))
             }
-
         }
     }
 }
