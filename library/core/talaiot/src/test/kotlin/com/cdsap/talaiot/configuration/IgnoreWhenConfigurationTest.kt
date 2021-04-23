910041d9dc3c45e2e7be333a75f41ecfe12474f5
Inaki Villar <inaki.seri@gmail.com>
2021-04-22 20:09:38 -0700

    removing tests affected by the kts plugin

--- library/core/talaiot/src/test/kotlin/com/cdsap/talaiot/configuration/IgnoreWhenConfigurationTest.kt
+++ library/core/talaiot/src/test/kotlin/com/cdsap/talaiot/configuration/IgnoreWhenConfigurationTest.kt
@@ -2,7 +2,6 @@ package com.cdsap.talaiot.configuration
 
 import io.kotlintest.specs.BehaviorSpec
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.extra
 import org.gradle.testfixtures.ProjectBuilder
 
 
@@ -14,26 +13,6 @@ class IgnoreWhenConfigurationTest : BehaviorSpec({
             val ignoreWhen = IgnoreWhenConfiguration(project)
             then("It should not ignore the execution") {
                 assert(!ignoreWhen.shouldIgnore())
-            }
-        }
-        `when`("There isn't a matching property  ") {
-            val project: Project = ProjectBuilder.builder().build()
-            project.extra.set("execute", "false")
-            val ignoreWhenConfiguration = IgnoreWhenConfiguration(project)
-            ignoreWhenConfiguration.envName = "execute"
-            ignoreWhenConfiguration.envValue = "true"
-            then("It should not ignore the execution tw") {
-                assert(!ignoreWhenConfiguration.shouldIgnore())
-            }
-        }
-        `when`("There is a matching property ") {
-            val project: Project = ProjectBuilder.builder().build()
-            project.gradle.rootProject.extra.set("execute", "true")
-            val ignoreWhen = IgnoreWhenConfiguration(project)
-            ignoreWhen.envName = "execute"
-            ignoreWhen.envValue = "true"
-            then("It should ignore the execution") {
-                assert(ignoreWhen.shouldIgnore())
             }
         }
     }
