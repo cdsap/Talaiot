a70a0bdb9e680c623e92f694ac5ded92302c5d2a
Inaki Villar <inaki.seri@gmail.com>
2022-03-24 20:35:56 -0700

    adding log for the failing test

--- library/plugins/talaiot-standard/src/test/kotlin/io/github/cdsap/talaiot/OutputPublisherBuildTest.kt
+++ library/plugins/talaiot-standard/src/test/kotlin/io/github/cdsap/talaiot/OutputPublisherBuildTest.kt
@@ -32,6 +32,7 @@ class OutputPublisherBuildTest : BehaviorSpec({
                 .withPluginClasspath()
                 .build()
             then("logs are shown in the output with the shrugged") {
+                println(result.output)
                 assert(result.output.contains("OutputPublisher"))
                 assert(result.output.contains("¯\\_(ツ)_/¯"))
                 assert(result.task(":assemble")?.outcome == TaskOutcome.SUCCESS)
