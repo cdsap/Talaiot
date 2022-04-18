a7ce640e913ec168b3f17e736d52203a5a75f932
Inaki Villar <inaki.seri@gmail.com>
2022-04-18 09:56:12 -0700

    fixing elastic search tests

--- library/plugins/elastic-search/elastic-search-publisher/build.gradle.kts
+++ library/plugins/elastic-search/elastic-search-publisher/build.gradle.kts
@@ -10,7 +10,7 @@ talaiotLib {
 
 dependencies {
     implementation(project(":library:core:talaiot"))
-    implementation("org.elasticsearch.client:elasticsearch-rest-high-level-client:7.3.0")
+    implementation("org.elasticsearch.client:elasticsearch-rest-high-level-client:7.17.2")
     implementation("com.google.code.gson:gson:2.8.5")
     testImplementation(project(":library:core:talaiot-test-utils"))
 }
