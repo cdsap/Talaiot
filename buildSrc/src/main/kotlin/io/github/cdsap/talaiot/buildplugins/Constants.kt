e7673477789d895982be8cb528ed9c615e48c79c
Iñaki Villar <inaki.seri@gmail.com>
2022-05-25 18:07:54 -0700

    bump snapshot version (#344)
    * bump snapshot version
    
    * reverting sample snapshot version
    
    * since 1.5.2 we weren't deploying snapshot

--- buildSrc/src/main/kotlin/io/github/cdsap/talaiot/buildplugins/Constants.kt
+++ buildSrc/src/main/kotlin/io/github/cdsap/talaiot/buildplugins/Constants.kt
@@ -1,7 +1,7 @@
 package io.github.cdsap.talaiot.buildplugins
 
 object Constants {
-    const val TALAIOT_VERSION = "1.5.3"
+    const val TALAIOT_VERSION = "1.5.4-SNAPSHOT"
     const val DEFAULT_GROUP_PLUGIN = "io.github.cdsap.talaiot.plugin"
     const val DEFAULT_GROUP_LIBRARY = "io.github.cdsap.talaiot"
 }
