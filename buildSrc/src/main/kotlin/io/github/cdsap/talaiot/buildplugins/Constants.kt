fcabc256ed987f0336f0295063813747bcbb4d83
Iñaki Villar <inaki.seri@gmail.com>
2022-05-25 17:11:50 -0700

    bump release 1.5.3 (#345)

--- buildSrc/src/main/kotlin/io/github/cdsap/talaiot/buildplugins/Constants.kt
+++ buildSrc/src/main/kotlin/io/github/cdsap/talaiot/buildplugins/Constants.kt
@@ -1,7 +1,7 @@
 package io.github.cdsap.talaiot.buildplugins
 
 object Constants {
-    const val TALAIOT_VERSION = "1.5.new-SNAPSHOT"
+    const val TALAIOT_VERSION = "1.5.3"
     const val DEFAULT_GROUP_PLUGIN = "io.github.cdsap.talaiot.plugin"
     const val DEFAULT_GROUP_LIBRARY = "io.github.cdsap.talaiot"
 }
