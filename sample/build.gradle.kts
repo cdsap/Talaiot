8e0aca92d1e8c67e939698d2b6bab9b9ae9d5e3e
Inaki Villar <inaki.seri@gmail.com>
2022-05-25 17:50:46 -0700

    since 1.5.2 we weren't deploying snapshot

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         maven(url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
     }
     dependencies {
-        classpath("io.github.cdsap:talaiot:1.5.3-SNAPSHOT")
+        classpath("io.github.cdsap:talaiot:1.5.2-SNAPSHOT")
     }
 }
 apply(plugin = "io.github.cdsap.talaiot")
