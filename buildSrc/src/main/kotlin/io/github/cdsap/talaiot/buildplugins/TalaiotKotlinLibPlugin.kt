5ad5589c394a166e7dbc9cbf5bbca92ef2328000
Inaki Villar <inaki.seri@gmail.com>
2022-06-14 11:03:20 +0700

    update sample and main versioning

--- buildSrc/src/main/kotlin/io/github/cdsap/talaiot/buildplugins/TalaiotKotlinLibPlugin.kt
+++ buildSrc/src/main/kotlin/io/github/cdsap/talaiot/buildplugins/TalaiotKotlinLibPlugin.kt
@@ -33,6 +33,12 @@ class TalaiotKotlinLibPlugin : Plugin<Project> {
         target.setUpJacoco()
         target.setUpJunitPlatform()
         target.setUpKtlint()
+
+        target.tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
         target.afterEvaluate {
             val extension = extensions.getByType<BaseConfiguration>()
             setProjectVersion(extension.version)
