8933fbf82f6e25e654b5a2d426d2a4b1ffae75c8
Inaki Villar <inaki.seri@gmail.com>
2023-09-12 20:53:52 -0700

    format metrics

--- library/core/talaiot/src/main/kotlin/io/github/cdsap/talaiot/metrics/GitMetrics.kt
+++ library/core/talaiot/src/main/kotlin/io/github/cdsap/talaiot/metrics/GitMetrics.kt
@@ -3,12 +3,11 @@ package io.github.cdsap.talaiot.metrics
 import io.github.cdsap.valuesourceprocess.CommandLineWithOutputValue
 import org.gradle.api.Project
 
-
 class GitBranchMetric(val project: Project) : SimpleMetric<String>(
     provider = {
         project.providers.of(CommandLineWithOutputValue::class.java) {
             it.parameters.commands.set("git rev-parse --abbrev-ref HEAD")
-        }.get().replace("\n","")
+        }.get().replace("\n", "")
     },
     assigner = { report, value -> report.environment.gitBranch = value }
 )
@@ -17,7 +16,7 @@ class GitUserMetric(val project: Project) : SimpleMetric<String>(
     provider = {
         project.providers.of(CommandLineWithOutputValue::class.java) {
             it.parameters.commands.set("git config --get user.name")
-        }.get().replace("\n","")
+        }.get().replace("\n", "")
     },
     assigner = { report, value -> report.environment.gitUser = value }
 )
