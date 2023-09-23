6714a589714004568335b711c19e2c71d2e5f5a9
Inaki Villar <inaki.seri@gmail.com>
2023-09-23 09:17:12 -0700

    update new snapshot sample

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         maven(url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
     }
     dependencies {
-        classpath("io.github.cdsap:talaiot:2.0.1-SNAPSHOT")
+        classpath("io.github.cdsap:talaiot:2.0.3-SNAPSHOT")
     }
 }
 apply(plugin = "io.github.cdsap.talaiot")
