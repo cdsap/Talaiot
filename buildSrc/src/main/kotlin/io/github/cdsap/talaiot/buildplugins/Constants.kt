70e465a9aace061150b70a3958322a215822aa2d
Inaki Villar <inaki.seri@gmail.com>
2023-09-30 08:19:33 -0700

    new release 2.0.3

--- buildSrc/src/main/kotlin/io/github/cdsap/talaiot/buildplugins/Constants.kt
+++ buildSrc/src/main/kotlin/io/github/cdsap/talaiot/buildplugins/Constants.kt
@@ -1,7 +1,7 @@
 package io.github.cdsap.talaiot.buildplugins
 
 object Constants {
-    const val TALAIOT_VERSION = "2.0.3-SNAPSHOT"
+    const val TALAIOT_VERSION = "2.0.4-SNAPSHOT"
     const val DEFAULT_GROUP_PLUGIN = "io.github.cdsap.talaiot.plugin"
     const val DEFAULT_GROUP_LIBRARY = "io.github.cdsap.talaiot"
 }
