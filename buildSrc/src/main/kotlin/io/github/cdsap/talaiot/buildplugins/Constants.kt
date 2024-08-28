bf3fd737907f0ca2fa4196bc30a585826949ef1f
Iñaki Villar <inaki.seri@gmail.com>
2024-08-28 10:00:11 +0200

    Preparing Next version (#402)
    * next version
    
    * preparing next version

--- buildSrc/src/main/kotlin/io/github/cdsap/talaiot/buildplugins/Constants.kt
+++ buildSrc/src/main/kotlin/io/github/cdsap/talaiot/buildplugins/Constants.kt
@@ -1,7 +1,7 @@
 package io.github.cdsap.talaiot.buildplugins
 
 object Constants {
-    const val TALAIOT_VERSION = "2.0.5-SNAPSHOT"
+    const val TALAIOT_VERSION = "2.0.6-SNAPSHOT"
     const val DEFAULT_GROUP_PLUGIN = "io.github.cdsap.talaiot.plugin"
     const val DEFAULT_GROUP_LIBRARY = "io.github.cdsap.talaiot"
 }
