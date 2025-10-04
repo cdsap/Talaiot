b198e63786de4858be5df31e5270498075301b2d
Iñaki Villar <inaki.seri@gmail.com>
2025-10-04 12:04:26 -0700

    Adding correct repo (#426)
    * Adding renovate
    
    * addign correct url

--- sample/settings.gradle.kts
+++ sample/settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     repositories {
-        maven(url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        maven(url = uri("https://central.sonatype.com/repository/maven-snapshots/"))
         mavenCentral()
         gradlePluginPortal()
     }
