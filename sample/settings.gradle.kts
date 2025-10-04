756a2ad2c697e741065ef3e78c16b06ed3d51d05
Iñaki Villar <inaki.seri@gmail.com>
2025-10-04 11:40:29 -0700

    prepare new release (#421)
    * prepare new release
    
    * Update Constants.kt

--- sample/settings.gradle.kts
+++ sample/settings.gradle.kts
@@ -3,7 +3,5 @@ pluginManagement {
         maven(url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
         mavenCentral()
         gradlePluginPortal()
-        maven(url = "https://jitpack.io")
-
     }
 }
